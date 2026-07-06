6583b180aba9aea2f44d2975261249562b390b81
disdreamq <vlaseyar@gmail.com>
2026-07-06 17:36:17 +0300

    refactoring

--- migrations/000001_init_users_table.up.sql
+++ migrations/000001_init_users_table.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
