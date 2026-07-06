8b99eac624c7d20ae179c7dc20ecba67c69e6e66
disdreamq <vlaseyar@gmail.com>
2026-07-06 18:57:13 +0300

    refactoring

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 
@@ -13,12 +14,16 @@ type UserService struct {
 	hasher   port.Hasher
 }
 
-func (u *UserService) CreateUser(username, email, password string) (int64, error) {
+func (u *UserService) CreateUser(ctx context.Context, username, email, password string) (int64, error) {
 	passwordHash, err := u.hasher.Hash(password)
 	if err != nil {
 		return -1, nil
 	}
-	id, err := u.userRepo.CreateUser(username, email, passwordHash)
+	domainUser, err := domain.NewUser(username, email, passwordHash)
+	if err != nil {
+		return -1, err
+	}
+	id, err := u.userRepo.CreateUser(ctx, domainUser.Username, domainUser.Email, domainUser.PasswordHash)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return -1, ErrUserAlreadyExists
@@ -27,27 +32,27 @@ func (u *UserService) CreateUser(username, email, password string) (int64, error
 	return id, nil
 }
 
-func (u *UserService) GetUserByID(userID int64) (*domain.User, error) {
-	user, err := u.userRepo.GetUserByID(userID)
+func (u *UserService) GetUserByID(ctx context.Context, userID int64) (*domain.User, error) {
+	user, err := u.userRepo.GetUserByID(ctx, userID)
 	if err != nil {
 		return nil, err
 	}
 	return user, nil
 }
 
-func (u *UserService) GetUserByEmail(email string) (*domain.User, error) {
-	user, err := u.userRepo.GetUserByEmail(email)
+func (u *UserService) GetUserByEmail(ctx context.Context, email string) (*domain.User, error) {
+	user, err := u.userRepo.GetUserByEmail(ctx, email)
 	if err != nil {
 		return nil, err
 	}
 	return user, nil
 }
 
-func (u *UserService) UpdateUser(user *domain.User) error {
-	return u.userRepo.UpdateUser(user)
+func (u *UserService) UpdateUser(ctx context.Context, user *domain.User) error {
+	return u.userRepo.UpdateUser(ctx, user)
 
 }
 
-func (u *UserService) DeleteUser(userID int64) error {
-	return u.userRepo.DeleteUser(userID)
+func (u *UserService) DeleteUser(ctx context.Context, userID int64) error {
+	return u.userRepo.DeleteUser(ctx, userID)
 }
