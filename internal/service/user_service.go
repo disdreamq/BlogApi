df7ff508b2b8c25cb01ef00945030b50af412a6c
disdreamq <vlaseyar@gmail.com>
2026-07-06 18:07:24 +0300

    added user service

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,6 +1,10 @@
 package service
 
 import (
+	"database/sql"
+	"errors"
+
+	"github.com/disdreamq/BlogApi/internal/domain"
 	"github.com/disdreamq/BlogApi/internal/port"
 )
 
@@ -16,7 +20,34 @@ func (u *UserService) CreateUser(username, email, password string) (int64, error
 	}
 	id, err := u.userRepo.CreateUser(username, email, passwordHash)
 	if err != nil {
-		return -1, err
+		if errors.Is(err, sql.ErrNoRows) {
+			return -1, ErrUserAlreadyExists
+		}
 	}
 	return id, nil
+}
+
+func (u *UserService) GetUserByID(userID int64) (*domain.User, error) {
+	user, err := u.userRepo.GetUserByID(userID)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (u *UserService) GetUserByEmail(email string) (*domain.User, error) {
+	user, err := u.userRepo.GetUserByEmail(email)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (u *UserService) UpdateUser(user *domain.User) error {
+	return u.userRepo.UpdateUser(user)
+
+}
+
+func (u *UserService) DeleteUser(userID int64) error {
+	return u.userRepo.DeleteUser(userID)
 }
