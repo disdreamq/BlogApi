817449f967feb3c48cbff62ad787bc0197328632
disdreamq <vlaseyar@gmail.com>
2026-07-07 15:26:07 +0300

    added errors for registr, user, port and unexp

--- internal/service/errors.go
+++ internal/service/errors.go
@@ -4,5 +4,19 @@ import "errors"
 
 var (
 	// Registration
-	ErrUserAlreadyExists = errors.New("User with this email already exists")
+	ErrEmptyPassword           = errors.New("Empty password not allowed.")
+	ErrInvalidPasswordLength   = errors.New("Invalid length of user password.")
+	ErrUserAlreadyExists       = errors.New("User with this email already exists.")
+	ErrCanNotCalculatePassHash = errors.New("Can not calculate password hash.")
+
+	// User
+	ErrUserNotFound = errors.New("User not found.")
+	ErrUserTimeOut  = errors.New("Time out error while processing user.")
+
+	// Post
+	ErrPostNotFound       = errors.New("Post not found.")
+	ErrLinkedUserNotFound = errors.New("Linked user not found.")
+
+	// Unexpected
+	ErrUnexpected = errors.New("Unexpected error.")
 )
