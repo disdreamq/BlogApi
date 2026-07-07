6d7f627552967df332c18dce8f38db7070ef2bfb
disdreamq <vlaseyar@gmail.com>
2026-07-07 15:26:20 +0300

    refactoring

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -9,15 +9,22 @@ import (
 	"github.com/disdreamq/BlogApi/internal/port"
 )
 
+// TODO логирование
 type UserService struct {
 	userRepo port.UserRepository
 	hasher   port.Hasher
 }
 
 func (u *UserService) CreateUser(ctx context.Context, username, email, password string) (int64, error) {
+	if password == "" {
+		return -1, ErrEmptyPassword
+	}
+	if len(password) < 8 || len(password) > 60 {
+		return -1, ErrInvalidPasswordLength
+	}
 	passwordHash, err := u.hasher.Hash(password)
 	if err != nil {
-		return -1, nil
+		return -1, ErrCanNotCalculatePassHash
 	}
 	domainUser, err := domain.NewUser(username, email, passwordHash)
 	if err != nil {
@@ -35,7 +42,12 @@ func (u *UserService) CreateUser(ctx context.Context, username, email, password 
 func (u *UserService) GetUserByID(ctx context.Context, userID int64) (*domain.User, error) {
 	user, err := u.userRepo.GetUserByID(ctx, userID)
 	if err != nil {
-		return nil, err
+		switch err {
+		case sql.ErrNoRows:
+			return nil, ErrUserNotFound
+		default:
+			return nil, ErrUnexpected
+		}
 	}
 	return user, nil
 }
@@ -43,16 +55,42 @@ func (u *UserService) GetUserByID(ctx context.Context, userID int64) (*domain.Us
 func (u *UserService) GetUserByEmail(ctx context.Context, email string) (*domain.User, error) {
 	user, err := u.userRepo.GetUserByEmail(ctx, email)
 	if err != nil {
-		return nil, err
+		switch err {
+		case sql.ErrNoRows:
+			return nil, ErrUserNotFound
+		default:
+			return nil, ErrUnexpected
+		}
 	}
 	return user, nil
 }
 
 func (u *UserService) UpdateUser(ctx context.Context, user *domain.User) error {
-	return u.userRepo.UpdateUser(ctx, user)
-
+	err := u.userRepo.UpdateUser(ctx, user)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return ErrUserNotFound
+		case sql.ErrTxDone:
+			return ErrUserTimeOut
+		default:
+			return ErrUnexpected
+		}
+	}
+	return nil
 }
 
 func (u *UserService) DeleteUser(ctx context.Context, userID int64) error {
-	return u.userRepo.DeleteUser(ctx, userID)
+	err := u.userRepo.DeleteUser(ctx, userID)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return ErrUserNotFound
+		case sql.ErrTxDone:
+			return ErrUserTimeOut
+		default:
+			return ErrUnexpected
+		}
+	}
+	return nil
 }
