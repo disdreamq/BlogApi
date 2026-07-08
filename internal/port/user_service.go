48f2668be7a98187f155cd387465503373ce9d26
disdreamq <vlaseyar@gmail.com>
2026-07-08 17:06:42 +0300

    refactoring

--- internal/port/user_service.go
+++ internal/port/user_service.go
@@ -10,6 +10,6 @@ type UserService interface {
 	CreateUser(ctx context.Context, username, email, password string) (*domain.User, error)
 	GetUserByID(ctx context.Context, userID int64) (*domain.User, error)
 	GetUserByEmail(ctx context.Context, email string) (*domain.User, error)
-	UpdateUser(ctx context.Context, user *domain.User) error
+	UpdateUser(ctx context.Context, username, email, password string) error
 	DeleteUser(ctx context.Context, userID int64) error
 }
