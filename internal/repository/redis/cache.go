9de7e75e237b363b1c1873e293c08a4850e26a8f
disdreamq <vlaseyar@gmail.com>
2026-07-13 15:44:53 +0300

    added logging

--- internal/repository/redis/cache.go
+++ internal/repository/redis/cache.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog/log"
 )
 
 type RedisCache struct {
@@ -19,16 +20,57 @@ func NewRedisCache(rdb *redis.Client) *RedisCache {
 	return &RedisCache{rdb: rdb}
 }
 
-func (r *RedisCache) Get(ctx context.Context, key string) (string, error) {
-	return r.rdb.Get(ctx, key).Result()
+func (r *RedisCache) Get(ctx context.Context, key string) (string, bool) {
+	val, err := r.rdb.Get(ctx, key).Result()
+	logger := log.Ctx(ctx)
+	if err != nil {
+		switch err {
+		case redis.Nil:
+			return val, false
+		default:
+			logger.Err(err).
+				Str("key", key)
+		}
+	}
+
+	hit := val != ""
+	if hit {
+		r.cacheMiss.Add(1)
+	} else {
+		r.cacheHit.Add(1)
+	}
+	logger.Debug().
+		Str("key", key).
+		Bool("hit", hit)
+	return val, true
 }
 
-func (r *RedisCache) Set(ctx context.Context, key string, value any, ttl time.Duration) error {
-	return r.rdb.Set(ctx, key, value.([]byte), ttl).Err()
+func (r *RedisCache) Set(ctx context.Context, key string, value any, ttl time.Duration) bool {
+	logger := log.Ctx(ctx)
+	err := r.rdb.Set(ctx, key, value.([]byte), ttl).Err()
+	if err != nil {
+		logger.Err(err).
+			Str("key", key)
+		return false
+	}
+	logger.Debug().
+		Str("key", key).
+		Msg("Set to cache")
+	return true
 }
 
-func (r *RedisCache) Del(ctx context.Context, key string) error {
-	return r.rdb.Del(ctx, key).Err()
+func (r *RedisCache) Del(ctx context.Context, key string) bool {
+	logger := log.Ctx(ctx)
+	err := r.rdb.Del(ctx, key).Err()
+	if err != nil {
+		logger.Err(err).
+			Str("key", key)
+		return false
+	}
+	logger.Debug().
+		Str("key", key).
+		Msg("Deleted from cache")
+	return true
 }
 
 func (r *RedisCache) ShowRatio() (float64, error) {
