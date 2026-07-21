5b70919c4febd3660f3a96fa38cff771ae4a171a
disdreamq <vlaseyar@gmail.com>
2026-07-21 15:44:39 +0300

    added returns to stop unauth request

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -28,11 +28,13 @@ func (m *AuthMiddleware) Authenticate(next http.Handler) http.Handler {
 		parts := strings.Split(authHeader, " ")
 		if len(parts) != 2 || parts[0] != "Bearer" {
 			http.Error(w, "invalid authorization header format", http.StatusUnauthorized)
+			return
 		}
 		token := parts[1]
 		payload, err := m.tokenProvider.ValidateToken(token)
 		if err != nil {
 			http.Error(w, "invalid token", http.StatusUnauthorized)
+			return
 		}
 		ctx := context.WithValue(r.Context(), "userID", payload.Claims.UserID)
 		ctx = context.WithValue(ctx, "email", payload.Claims.Email)
