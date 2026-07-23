3fbf989eec085951a03c2161aecb31187ed8a627
disdreamq <vlaseyar@gmail.com>
2026-07-23 17:20:20 +0300

    refactoring

--- internal/service/post.go
+++ internal/service/post.go
@@ -127,7 +127,7 @@ func (p *PostService) UpdateWithValidate(ctx context.Context, currUserID, postID
 	if err != nil {
 		switch err {
 		case sql.ErrNoRows:
-			return ErrPostNotFound
+			return ErrUpdatePostFailed
 		default:
 			return ErrUnexpected
 		}
@@ -153,7 +153,7 @@ func (p *PostService) Update(ctx context.Context, postID int64, title, content s
 	if err != nil {
 		switch err {
 		case sql.ErrNoRows:
-			return ErrUpdatePostFailed
+			return ErrPostNotFound
 		default:
 			return ErrUnexpected
 		}
