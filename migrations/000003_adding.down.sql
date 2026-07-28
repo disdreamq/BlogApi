f27af0635da4ddf2e1180d2dc45f2c7dad2cdc29
disdreamq <vlaseyar@gmail.com>
2026-07-28 16:56:03 +0300

    added comment to run migration outside transaction

--- migrations/000003_adding.down.sql
+++ migrations/000003_adding.down.sql
@@ -1,2 +1,3 @@
-DROP INDEX CONCURRENTLY idx_users_email;
-DROP INDEX CONCURRENTLY idx_posts_title_user_id;
+-- +migrate NoTransaction
+DROP INDEX CONCURRENTLY idx_users_email
+DROP INDEX CONCURRENTLY idx_posts_title_user_id
--- migrations/000003_adding.up.sql
+++ migrations/000003_adding.up.sql
@@ -1,2 +1,3 @@
+-- +migrate NoTransaction
 CREATE INDEX CONCURRENTLY idx_users_email ON users (email);
-CREATE INDEX CONCURRENTLY idx_posts_title_user_id ON users (title, user_id);
+CREATE INDEX CONCURRENTLY idx_posts_title_user_id ON posts (title, user_id);
